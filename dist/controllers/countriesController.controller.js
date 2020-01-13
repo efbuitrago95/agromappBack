2b57d44f1814457c5654ac2fd7301b426bdc65c9
efbuitrago95 <JUANANO190216>
2020-01-13 09:24:37 -0500

    lista de paises y agregar campo de imagen en el modelo de la base de datos

--- dist/controllers/countriesController.controller.js
+++ dist/controllers/countriesController.controller.js
@@ -40,15 +40,42 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var countries_model_1 = __importDefault(require("../models/countries.model"));
+var utils_1 = require("../utils");
+var numberItems = 10;
 var CountriesController = /** @class */ (function () {
     function CountriesController() {
         var _this = this;
         this.getAll = function (req, res) { return __awaiter(_this, void 0, void 0, function () {
+            var querySql;
             return __generator(this, function (_a) {
-                countries_model_1.default.query()
-                    .eager("[languages]")
+                querySql = countries_model_1.default.query().eager('languages');
+                if (req.query.search) {
+                    querySql.where("name", "like", "%" + req.query.search + "%");
+                }
+                if (req.query.language) {
+                    querySql.where("idLanguage", req.query.language);
+                }
+                if (req.query.page) {
+                    querySql.page(Number(req.query.page) - 1, numberItems);
+                }
+                querySql
                     .then(function (data) {
-                    res.status(200).send(data);
+                    var dataResponse = {};
+                    var paginationData = {};
+                    if (req.query.page) {
+                        paginationData = utils_1.Utils.generatePaging(numberItems, req.query.page, data);
+                        dataResponse = {
+                            results: data.results,
+                            paginationData: paginationData
+                        };
+                    }
+                    else {
+                        dataResponse = {
+                            results: data,
+                            paginationData: paginationData
+                        };
+                    }
+                    res.status(200).send(dataResponse);
                 })
                     .catch(function (error) {
                     res.status(200).send(error);
--- src/controllers/countriesController.controller.ts
+++ src/controllers/countriesController.controller.ts
@@ -1,60 +1,92 @@
-import { Request, Response } from "express";
+import {Request, Response} from "express";
 import CountriesModel from "../models/countries.model";
+import languagesModel from "../models/languages.model";
+import {Utils} from "../utils";
 
+const numberItems = 10;
 export default class CountriesController {
-  public getAll = async (req: Request, res: Response) => {
-    CountriesModel.query()
-      .eager("[languages]")
-      .then(data => {
-        res.status(200).send(data);
-      })
-      .catch((error: any) => {
-        res.status(200).send(error);
-      });
-  };
+    public getAll = async (req: Request, res: Response) => {
+        let querySql = CountriesModel.query().eager('languages');
+        if (req.query.search) {
+            querySql.where("name", "like", `%${req.query.search}%`);
+        }
 
-  public create = async (req: Request, res: Response) => {
-    CountriesModel.query()
-      .insert(req.body)
-      .then(data => {
-        res.status(200).send(data);
-      })
-      .catch((e: any) => {
-        res.status(400).send(e);
-      });
-  };
+        if (req.query.language) {
+            querySql.where("idLanguage", req.query.language);
+        }
 
-  public getById = async (req: Request, res: Response) => {
-    CountriesModel.query()
-      .eager("[languages]")
-      .findById(req.params.id)
-      .then(data => {
-        res.status(200).send(data);
-      })
-      .catch((error: any) => {
-        res.status(200).send(error);
-      });
-  };
+        if (req.query.page) {
+            querySql.page(Number(req.query.page) - 1, numberItems);
+        }
+        querySql
+            .then((data: any) => {
+                let dataResponse = {};
+                let paginationData = {};
+                if (req.query.page) {
+                    paginationData = Utils.generatePaging(
+                        numberItems,
+                        req.query.page,
+                        data
+                    );
+                    dataResponse = {
+                        results: data.results,
+                        paginationData: paginationData
+                    };
+                } else {
+                    dataResponse = {
+                        results: data,
+                        paginationData: paginationData
+                    };
+                }
+                res.status(200).send(dataResponse);
+            })
+            .catch((error: any) => {
+                res.status(200).send(error);
+            });
+    };
 
-  public update = async (req: Request, res: Response) => {
-    CountriesModel.query()
-      .patchAndFetchById(req.body.id, req.body)
-      .then(res1 => {
-        res.status(200).send(res1);
-      })
-      .catch((e: any) => {
-        res.status(200).send(e);
-      });
-  };
+    public create = async (req: Request, res: Response) => {
+        CountriesModel.query()
+            .insert(req.body)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch((e: any) => {
+                res.status(400).send(e);
+            });
+    };
 
-  public deletedbyId = async (req: Request, res: Response) => {
-    CountriesModel.query()
-      .deleteById(req.params.id)
-      .then(data => {
-        res.status(200).send(data);
-      })
-      .catch((error: any) => {
-        res.status(200).send(error);
-      });
-  };
+    public getById = async (req: Request, res: Response) => {
+        CountriesModel.query()
+            .eager("[languages]")
+            .findById(req.params.id)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch((error: any) => {
+                res.status(200).send(error);
+            });
+    };
+
+    public update = async (req: Request, res: Response) => {
+        CountriesModel.query()
+            .patchAndFetchById(req.body.id, req.body)
+            .then(res1 => {
+                res.status(200).send(res1);
+            })
+            .catch((e: any) => {
+                res.status(200).send(e);
+            });
+    };
+
+    public deletedbyId = async (req: Request, res: Response) => {
+        CountriesModel.query()
+            .deleteById(req.params.id)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch((error: any) => {
+                res.status(200).send(error);
+            });
+    };
 }
